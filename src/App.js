7cb6649a284338208cfcbe0da7b96050b0bf8dde
tomlikescode <gtmallen@gmail.com>
2021-12-28 14:12:15 -0600

    12-28

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Years from './Years'
 export default App
 
 
-const imagesAndColors = [{image: '../images/pizza.jpg', color: 'red', Year: '2019', info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati, beatae non earum porro libero nesciunt et saepe sapiente voluptates illum?'},
+const imagesAndColors = [{image: './images/pizza.jpg', color: 'red', Year: '2019', info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati, beatae non earum porro libero nesciunt et saepe sapiente voluptates illum?'},
                         {image: '../images/pizza.jpg', color: 'orange', Year: '2018', info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati, beatae non earum porro libero nesciunt et saepe sapiente voluptates illum?'},
                         {image: '../images/pizza.jpg', color: 'yellow', Year: '2017', info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati, beatae non earum porro libero nesciunt et saepe sapiente voluptates illum?'},
                         {image: '../images/pizza.jpg', color: 'green', Year: '2016', info: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati, beatae non earum porro libero nesciunt et saepe sapiente voluptates illum?'},
